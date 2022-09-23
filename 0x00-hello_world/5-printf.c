1b7267801e2f63cc4cc98ac8d07fb42712e6bf4e
PeterAnami <andakepe@ueab.ac.ke>
2022-09-23 03:56:53 -0700

    added new file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/**
+ * main - Program start function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
