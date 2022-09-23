1ddf0d3c670790e28ba6a05b515da313eb0a1ba3
PeterAnami <andakepe@ueab.ac.ke>
2022-09-23 03:44:50 -0700

    new file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Program start function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
